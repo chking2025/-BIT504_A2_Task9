544d21f8c090f6eed83bdbc2a094032c9c5da51b
BIT504 <cynergykinghuaki@myop.ac.nz>
2025-12-17 07:56:59 +1300

    Initial upload of project

---
+++ BIT504_A2_Task9/src/bit504/MainProgram.java
@@ -0,0 +1,13 @@
+package bit504;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Task 9 of BIT504: Assignment 2");
+		System.out.println("You have reached the end of your assignment, congrats!");
+
+	
+	}
+
+}
