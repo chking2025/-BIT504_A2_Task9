1ae69f1bf5ad1d0a9f20e80e3b1ddb3489474873
BIT504 <cynergykinghuaki@myop.ac.nz>
2025-12-17 08:14:06 +1300

    Revert "Added a newMethod method to the program"
    This reverts commit fba7f221886ed705de5257ecea1bceb5860408d9.

--- BIT504_A2_Task9/src/bit504/MainProgram.java
+++ BIT504_A2_Task9/src/bit504/MainProgram.java
@@ -6,17 +6,8 @@ public class MainProgram {
 		
 		System.out.println("Welcome to Task 9 of BIT504: Assignment 2");
 		System.out.println("You have reached the end of your assignment, congrats!");
-		
-		newMethod();
 
 	
-	} // end of main method
-	
-	private static void newMethod () {
-		
-		System.out.println("I am a new method.");
-		
-		
 	}
 
-} // end of Main class
+}
